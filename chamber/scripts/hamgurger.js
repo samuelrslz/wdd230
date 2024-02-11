a7f3af000bf03ffbbe63e2b08a0d4f591a1dd79e
samuelrslz <rioslazosamuel@gmail.com>
2024-02-10 18:17:50 -0700

    Improved Home page chamber

--- chamber/scripts/hamgurger.js
+++ chamber/scripts/hamgurger.js
@@ -5,3 +5,10 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+const darkModeButton = document.querySelector('#dark-mode');
+
+darkModeButton.addEventListener('click', () => {
+	document.body.classList.toggle('darkMode');
+	darkModeButton.classList.toggle('whenDark');
+});
