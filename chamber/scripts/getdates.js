bb3cfc2f4ddc24b0f92fc529949c28b70d378861
samuelrslz <rioslazosamuel@gmail.com>
2024-03-02 14:30:11 -0700

    W08 prove

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,9 +1,15 @@
 // Grab the element with an id of #copyrightDate
-const copyrightDate = document.querySelector('#copyrightDate');
+const copyrightDate = document.querySelector("#copyrightDate");
 
 // Modify the innerHTML of that element to get the full year
 copyrightDate.innerHTML = new Date().getFullYear();
 
 // Grab the lastModified
-const lastModified = document.querySelector('#lastModified');
+const lastModified = document.querySelector("#lastModified");
 lastModified.innerHTML = `Last modified: ${document.lastModified}`;
+
+// This is for the form in the join page
+document.addEventListener("DOMContentLoaded", function () {
+  var timestampField = document.getElementById("timestamp");
+  timestampField.value = Date.now();
+});
