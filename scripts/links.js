5dcdb4a0d02ec3ef16db1071f7bc998c873aafc7
samuelrslz <rioslazosamuel@gmail.com>
2024-03-06 18:30:59 -0700

    finished json links

--- scripts/links.js
+++ scripts/links.js
@@ -1,12 +1,30 @@
 const baseURL = "https://samuelrslz.github.io/wdd230/";
-
 const linksURL = "https://samuelrslz.github.io/wdd230/data/links.json";
+const activitiesList = document.querySelector("#activitiesList");
 
 async function getLinks() {
   const response = await fetch(linksURL);
   const data = await response.json();
-  console.log(data);
-  //displayLinks(data.lessons);
+  displayLinks(data.lessons);
 }
 
 getLinks();
+
+const displayLinks = (weeks) => {
+  weeks.forEach((week) => {
+    let lessonNumber = week.lesson;
+    week.links.forEach((link) => {
+      let lessonURL = link.url;
+      let lessonName = link.title;
+
+      let listItem = document.createElement("li");
+      let linkElement = document.createElement("a");
+
+      linkElement.textContent = `${lessonNumber}: ${lessonName}`;
+      linkElement.setAttribute("href", lessonURL);
+
+      listItem.appendChild(linkElement);
+      activitiesList.appendChild(listItem);
+    });
+  });
+};
